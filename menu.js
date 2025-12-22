c13d5c70ddc77fe3855c7057a3fb518d5fa8f56f
kadukitesesi <kadukitesesi@gmail.com>
2025-12-22 20:46:15 -0300

    criacao do header

---
+++ menu.js
@@ -0,0 +1,12 @@
+let menu_list_mobile = document.getElementById("top_menu_list_mobile");
+let top_menu_list = document.querySelector(".top_menu_list");
+
+menu_list_mobile.addEventListener("click", () => {
+    if (top_menu_list.classList.contains("top_menu_list_none")) {
+        top_menu_list.classList.remove("top_menu_list_none");
+        top_menu_list.classList.add('top_menu_list_mobile');
+    } else {
+        top_menu_list.classList.remove('top_menu_list_mobile');
+        top_menu_list.classList.add("top_menu_list_none");
+    }
+});
