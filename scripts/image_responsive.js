56036018e772d0299dc618cc5b6cb7fab39d0c20
kadukitesesi <kadukitesesi@gmail.com>
2025-12-26 14:35:41 -0300

    fixed styles and scripts

--- scripts/image_responsive.js
+++ scripts/image_responsive.js
@@ -1,21 +1,21 @@
 const getBetter = document.getElementById("get_better_image");
 const timeBreak = document.getElementById("img_time_break");
-const path = "./assets/img/main/"
+const path = "./assets/img/";
 
-function verifyWidthScreen(image ,imageName, imageNameMobile) {
+function verifyWidthScreen(sub ,image, imageName, imageNameMobile) {
+    if (!image) return;
     const widthScreen = window.innerWidth;
-    if (widthScreen >= 765) {
-        image.src = `${path}${imageName}`;
-    } else {
-       image.src = `${path}${imageNameMobile}`;
+    if (widthScreen >= 765) { 
+        image.src = `${path}${sub}/${imageName}`; 
+    } else { 
+        image.src = `${path}${sub}/${imageNameMobile}`; 
     }
 }
 
 function updateImages() {
-    verifyWidthScreen(timeBreak,'planning.png', 'mobile_planning.png');
-    verifyWidthScreen(getBetter, 'get_better_image.png', 'get_better_image_mobile.png');   
+    verifyWidthScreen("time_break",timeBreak, 'planning.png', 'mobile_planning.png');
+    verifyWidthScreen("get_better",getBetter, 'get_better_image.png', 'get_better_image_mobile.png');   
 }
 
 updateImages();
-
 window.addEventListener("resize", updateImages);
